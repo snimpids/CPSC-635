57e8c154addc4a61a6b953873ecd24171a9459e2
snimpids <snimpids@gmail.com>
2017-01-18 17:43:04 -0700

    Add files via upload

--- assn1q3.m
+++ assn1q3.m
@@ -1,14 +1,24 @@
 clear;
 
-t = (0:2*pi/(1024-1):2*pi);
-x = (1:1024);
-f = @(t) sin(t)+randn(1,1024)/10;
+T = 1024;
+
+k = 1:T;
+
+t = (0:2*pi/(T-1):2*pi);
+x = (1:T);
+f = @(t) sin(t)+randn(1,T)/10;
 
 F = f(t);
 
-L = length(F);
-d = 1:L/2;
+% M = 0.5*[1,1,0,0;0,0,1,1;-1,1,0,0;0,0,-1,1];
+% 
+% H = repmat(M,3)
+H = zeros(16,16);
 
-% phi = (f(2*d-1)-f(2*d))/2;
-
-% plot(phi)
+for j = 1:2:16
+    for i = 1:8
+        if j == i+1 || i == j
+            H(i,j) = 1
+        end;
+    end;
+end;
